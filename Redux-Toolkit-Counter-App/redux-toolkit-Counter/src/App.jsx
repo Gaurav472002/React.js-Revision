7e81aa3de3ce5584c2972770d9963deb57519ee7
Gaurav472002 <sagarchakraborty472@gmail.com>
2025-11-14 11:23:11 +0530

    added code for counter app

---
+++ Redux-Toolkit-Counter-App/redux-toolkit-Counter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './components/Counter'
+import { useDispatch } from 'react-redux'
+
+function App() {
+  
+  const dispatch = useDispatch();
+  return (
+    <>
+      <button onClick={e =>dispatch({type: 'INCREMENT'})}>Increment</button>
+      <Counter />
+      <button onClick={e =>dispatch({type: 'DECREMENT'})}>Decrement</button>
+    </>
+  )
+}
+
+export default App
---
+++ Redux-Toolkit-Counter-App/redux-toolkit-Counter/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import { useSelector } from 'react-redux';
+
+const Counter = () => {
+
+    const count = useSelector (state => state);
+
+    return(
+        <div>
+            <h1>{count}</h1>
+        </div>
+    )
+}
+export default Counter;
